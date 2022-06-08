77a630570858f878446d29f869851082741f6513
klmichael <87207684+klmichael@users.noreply.github.com>
2022-06-08 17:19:31 -0500

    basic GET and POST calls

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+const { envelopes, createEnvelope, getEnvelope } = require('./server/db');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
   res.send("Hello World!");
+})
+
+app.get('/envelopes/', (req, res, next) => {
+  res.send(envelopes);
+})
+
+app.get('/envelopes/:envelopeId/', (req, res, next) => {
+  let id = req.params.envelopeId;
+  const selection = getEnvelope(id);
+  res.send(selection);
+})
+
+app.post('/envelopes/', (req, res, next) => {
+  const { name } = req.query;
+  const newEnvelope = createEnvelope(name);
+  res.status(201).send(newEnvelope);
 })
 
 const PORT = process.env.PORT || 3000;
--- server/db.js
+++ server/db.js
@@ -1,4 +1,48 @@
 //Envelopes
+var nextEnvelopeId = 5;
+
+const envelopes = [
+  {
+  "id": "1",
+  "name": "toys",
+  "amount": 0
+  },
+  {
+    "id": "2",
+    "name": "candy",
+    "amount": 0
+  },
+  {
+    "id": "3",
+    "name": "books",
+    "amount": 0
+  },
+  {
+    "id": "4",
+    "name": "stuffies",
+    "amount": 0
+  }
+];
+
+function createEnvelope(name) {
+  var newEnvelope = {
+    id: `${nextEnvelopeId}`,
+    name: name,
+    amount: 0,
+  };
+  envelopes.push(newEnvelope);
+  nextEnvelopeId++;
+  return envelopes[envelopes.length - 1];
+}
+
+function getEnvelope(envelopeId) {
+  let index = envelopes.findIndex(envelope => envelope.id === envelopeId);
+  if (index < 0) {
+    return 'Sorry, that is not a valid envelope.'
+  } else {
+    return envelopes[index];
+  }
+}
 
 //Transactions
 
@@ -7,3 +51,5 @@
 //Deposit function
 
 //Transfer function
+
+module.exports = { envelopes, createEnvelope, getEnvelope };
