b67c2daf48bf1d6faccdec4d7ec207b5e357b8f8
klmichael <87207684+klmichael@users.noreply.github.com>
2022-06-10 15:01:09 -0500

    PUT and DELETE endpoints

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const { envelopes, createEnvelope, getEnvelope } = require('./server/db');
+const { envelopes, createEnvelope, getEnvelope, deleteEnvelope, withdraw, deposit, transfer } = require('./server/db');
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
-  res.send("Hello World!");
+  res.send("Welcome to your envelopes budget!");
 })
 
 app.get('/envelopes/', (req, res, next) => {
@@ -17,13 +17,57 @@ app.get('/envelopes/', (req, res, next) => {
 app.get('/envelopes/:envelopeId/', (req, res, next) => {
   let id = req.params.envelopeId;
   const selection = getEnvelope(id);
-  res.send(selection);
+  res.send(selection); 
 })
 
 app.post('/envelopes/', (req, res, next) => {
   const { name } = req.query;
   const newEnvelope = createEnvelope(name);
   res.status(201).send(newEnvelope);
+})
+
+app.delete('/envelopes/:envelopeId/', (req, res, next) => {
+  let id = req.params.envelopeId;
+  const deletion = deleteEnvelope(id);
+  if (deletion) {
+    res.status(204);
+  } else {
+    res.status(500);
+  }
+  res.send();
+})
+
+app.put('/deposit/', (req, res, next) => {
+  let {envelopeId, amount} = req.body;
+  let transactionSuccess = deposit(envelopeId, amount);
+    if (transactionSuccess) {
+      res.status(200)
+    } else {
+      res.status(400)
+    }
+    res.send();
+})
+
+app.put('/withdraw/', (req, res, next) => {
+  let {envelopeId, amount} = req.body;
+  let transactionSuccess = withdraw(envelopeId, amount);
+    if (transactionSuccess) {
+      res.status(200)
+    } else {
+      res.status(400)
+    }
+    res.send();
+})
+
+app.put('/transfer/', (req, res, next) => {
+  let {envelopeId1, envelopeId2, amount}  = req.body;
+  let transactionSuccess = transfer(envelopeId1, envelopeId2, amount);
+    if (transactionSuccess) {
+      res.status(200)
+    } else {
+      res.status(400)
+    }
+    res.send();
 })
 
 const PORT = process.env.PORT || 3000;
--- server/db.js
+++ server/db.js
@@ -5,22 +5,22 @@ const envelopes = [
   {
   "id": "1",
   "name": "toys",
-  "amount": 0
+  "amount": 10
   },
   {
     "id": "2",
     "name": "candy",
-    "amount": 0
+    "amount": 5
   },
   {
     "id": "3",
     "name": "books",
-    "amount": 0
+    "amount": 10
   },
   {
     "id": "4",
     "name": "stuffies",
-    "amount": 0
+    "amount": 15
   }
 ];
 
@@ -35,21 +35,77 @@ function createEnvelope(name) {
   return envelopes[envelopes.length - 1];
 }
 
-function getEnvelope(envelopeId) {
+function findEnvelopeIndex(envelopeId) {
   let index = envelopes.findIndex(envelope => envelope.id === envelopeId);
-  if (index < 0) {
-    return 'Sorry, that is not a valid envelope.'
-  } else {
-    return envelopes[index];
+  return index; //-1 or 0+
+}
+
+function validIndex(index) {
+  return index >= 0; //boolean
+}
+
+function amountAvailable(validIndex, amount) {
+  let available = envelopes[validIndex].amount >= amount;
+  return available;  //boolean
+}
+
+function getEnvelope(envelopeId) {
+  let index = findEnvelopeIndex(envelopeId);
+  if (validIndex(index)) {
+    return envelopes[index];  //specified envelope object
   }
 }
 
-//Transactions
+function deleteEnvelope(envelopeId) {
+  let index = findEnvelopeIndex(envelopeId);
+  if (validIndex(index)) {
+    return envelopes.splice(index, 1); //array with deleted envelope object
+  }
+}
 
-//Withdraw function
+function withdraw(envelopeId, amount) {
+  let index = findEnvelopeIndex(envelopeId);
+  //First, validate the index
+  if (!validIndex(index)) {
+    return false;
+    //If the index is valid, check the amount is available
+  } else if (!amountAvailable(index, amount)) {
+    return false
+    //Both index and amount must be valid to get to the final statement
+  } else {
+    envelopes[index].amount -= amount;
+    return true;
+  } 
+}
+      
 
 //Deposit function
+function deposit(envelopeId, amount) {
+  let index = findEnvelopeIndex(envelopeId);
+  if (!validIndex(index)) {
+    return false;
+  } else {
+    envelopes[index].amount += amount;
+    return true;
+  }
+}
 
 //Transfer function
+function transfer(envelopeId1, envelopeId2, amount) {
+  let index1 = findEnvelopeIndex(envelopeId1);
+  let index2 = findEnvelopeIndex(envelopeId2);
+  //Verify that both envelopes exist
+  if (!validIndex(index1) || !validIndex(index2)) {
+    return false;
+  //If both envelopes exist, verify that the first envelope has sufficient funds.
+  } else if (!amountAvailable(index1, amount)) {
+    return false;
+  //If the first envelope has suffcient funds, perform the transaction;
+  } else {
+    envelopes[index1].amount -= amount;
+    envelopes[index2].amount += amount;
+    return true;
+  }
+}
 
-module.exports = { envelopes, createEnvelope, getEnvelope };
+module.exports = { envelopes, createEnvelope, getEnvelope, deleteEnvelope, withdraw, deposit, transfer };
